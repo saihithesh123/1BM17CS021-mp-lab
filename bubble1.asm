6a80c3341a8cb5e46c653c7c46a38677808d7561
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2019-09-13 14:08:16 +0530

    Add files via upload

---
+++ bubble1.asm
@@ -0,0 +1,25 @@
+.model small
+.data
+   n db 5
+   a db 5,7,4,3,6
+.code
+   mov ax,@data
+   mov ds,ax
+   mov cl,n
+   dec cl
+outloop:mov ch,cl
+        mov si,0
+inloop:mov al,a[si]
+       inc si
+       cmp al,a[si]
+       jb noxg
+       xchg al,a[si]
+       mov a[si-1],al
+noxg:dec ch
+     JNZ inloop
+     dec cl
+     JNZ outloop
+     mov ah,4ch
+     int 21h
+     end
+
